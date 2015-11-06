517524d5b10ccdde56978182a5b52d48cbf7a859
vyara <vyara_hristova@yahoo.de>
2015-11-06 21:32:14 +0200

    added ObjectFactory.cs in Infrastructure

---
+++ TownSystem/TownSystem.Services/App_Start/DatabaseConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace TownSystem.Services
+{
+    using System.Data.Entity;
+    using Data;
+    using Data.Migrations;
+
+    public class DatabaseConfig
+    {
+        public static void Initialize()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<TownSystemDbContext, Configuration>());
+            TownSystemDbContext.Create().Database.Initialize(true);
+        }
+    }
+}
---
+++ TownSystem/TownSystem.Services/Infrastructure/ObjectFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace TownSystem.Services.Infrastructure
+{
+    using Ninject;
+
+    public static class ObjectFactory
+    {
+        private static IKernel savedKernel;
+
+        public static void Initialize(IKernel kernel)
+        {
+            savedKernel = kernel;
+        }
+
+        public static T Get<T>()
+        {
+            return savedKernel.Get<T>();
+        }
+    }
+}
