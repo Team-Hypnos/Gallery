2c299b483402f6562110f7b7bf2ea568108b3883
vyara <vyara_hristova@yahoo.de>
2015-11-08 13:07:50 +0200

    changed Token rout to /api/Account/Login - for login

--- TownSystem/TownSystem.Services/App_Start/Startup.cs
+++ TownSystem/TownSystem.Services/App_Start/Startup.cs
@@ -31,7 +31,7 @@
             PublicClientId = "self";
             OAuthOptions = new OAuthAuthorizationServerOptions
             {
-                TokenEndpointPath = new PathString("/Token"),
+                TokenEndpointPath = new PathString("/api/Account/Login"),
                 Provider = new ApplicationOAuthProvider(PublicClientId),
                 AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
                 AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
