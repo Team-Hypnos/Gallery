6ab438f23afec106896140c607d74b77ec1dac9e
vyara <vyara_hristova@yahoo.de>
2015-11-13 22:33:46 +0200

    added user and UserId to Comments db model

--- TownSystem/TownSystem.Models/Comment.cs
+++ TownSystem/TownSystem.Models/Comment.cs
@@ -17,6 +17,10 @@
 
         public bool IsDeleted { get; set; }
 
+         public int UserId { get; set; }
+
+        public virtual User User { get; set; }
+
         public virtual int? PostId { get; set; }
 
         public virtual Post Post { get; set; }
