5f9ea325f75b79e7e17eea51fb90c7c487a30eed
vyara <vyara_hristova@yahoo.de>
2015-11-14 21:43:54 +0200

    added ICommentsService.cs

---
+++ TownSystem/TownSystem.Services.Data/Contracts/ICommentsService.cs
@@ -0,0 +1,23 @@
+﻿namespace TownSystem.Services.Data.Contracts
+{
+    using System;
+    using System.Linq;
+    using Models;
+
+
+    public interface ICommentsService
+    {
+        IQueryable<Comment> ById(int id);
+
+        IQueryable<Comment> PostComments(int? postId);
+
+        IQueryable<Comment> UserComments(string username);
+
+        int Add(int id, string content, string username, DateTime timePosted);
+
+        int Edit(int id, string content, string username);
+
+        int Delete(int id);
+
+    }
+}
--- TownSystem/TownSystem.Services/Controllers/CommentsController.cs
+++ TownSystem/TownSystem.Services/Controllers/CommentsController.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-
-namespace TownSystem.Services.Controllers
+﻿namespace TownSystem.Services.Controllers
 {
+    using System.Web.Http;
+
     public class CommentsController : ApiController
     {
     }
