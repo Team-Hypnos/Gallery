a9ef002f2b7b79004ac060da9d0422f96188b064
vyara <vyara_hristova@yahoo.de>
2015-11-19 00:29:07 +0200

    added one route test for comments conttoller

---
+++ TownSystem/TownSystem.Services.Tests/RouteTests/CommentsControllerTests.cs
@@ -0,0 +1,36 @@
+﻿namespace TownSystem.Services.Tests.RouteTests
+{
+    using System.Net.Http;
+    using System.Web.Http;
+    using Controllers;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Models.Comment;
+    using MyTested.WebApi;
+
+    [TestClass]
+    public class CommentsControllerTests
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            var config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            MyWebApi.IsUsing(WebApiConfig.Config);
+        }
+
+        [TestMethod]
+        public void ControllerShouldMapCorrectly()
+        {
+            MyWebApi
+                .Routes()
+                .ShouldMap("/api/Comments")
+                .WithHttpMethod(HttpMethod.Post)
+                .WithJsonContent(@"{""Content"": ""Test"", ""UserName"" : ""TestUser""}")
+                .To<CommentsController>(c => c.Post(new CommentDetalsRequestModel
+                {
+                    Content = "Test",
+                    UserName = "TestUser"
+                }));
+        }
+    }
+}
