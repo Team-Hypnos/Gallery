1171b98569962ff7d71473fb9b2cbd925f9dec4b
vyara <vyara_hristova@yahoo.de>
2015-11-18 23:56:32 +0200

    renamed Api.Tests to Services.Tests for consistency

--- TownSystem/TownSystem.Services.Tests/Properties/AssemblyInfo.cs
+++ TownSystem/TownSystem.Services.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TownSystem.Api.Tests")]
+[assembly: AssemblyTitle("TownSystem.Services.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TownSystem.Api.Tests")]
+[assembly: AssemblyProduct("TownSystem.Services.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
