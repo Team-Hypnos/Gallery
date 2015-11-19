2031328cf077a274c8ccd1e62d1158de1ff671ba
ReniGetskova <reni.getskova@gmail.com>
2015-11-20 01:09:33 +0200

    Implemented LikesController. Can already like and dislike post!

--- TownSystem/TownSystem.Services/Controllers/LikesController.cs
+++ TownSystem/TownSystem.Services/Controllers/LikesController.cs
@@ -1,13 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-
-namespace TownSystem.Services.Controllers
+﻿namespace TownSystem.Services.Controllers
 {
+    using System.Web.Http;
+    using TownSystem.Services.Data.Contracts;
+
     public class LikesController : ApiController
     {
+        private readonly ILikesService likes;
+        //private readonly IUsersService users;
+
+        public LikesController(ILikesService likes, IUsersService users)
+        {
+            this.likes = likes;
+            //this.users = users;
+        }
+
+        // POST(authorize) api/likes/{postId}
+        [Authorize]
+        [HttpPost]
+        [Route("api/like/{id}")]
+        public IHttpActionResult Like(int id)
+        {
+            if (this.likes.PostIsLikedByUser(id, this.User.Identity.Name))
+            {
+                return this.BadRequest("You already have liked this project.");
+            }
+
+            this.likes.LikePost(id, this.User.Identity.Name);
+            return this.Ok();
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("api/dislike/{id}")]
+        public IHttpActionResult Dislike(int id)
+        {
+            if (!this.likes.PostIsLikedByUser(id, this.User.Identity.Name))
+            {
+                return this.BadRequest("You have not yet liked this project.");
+            }
+
+            this.likes.DislikePost(id, this.User.Identity.Name);
+            return this.Ok();
+        }
     }
 }
